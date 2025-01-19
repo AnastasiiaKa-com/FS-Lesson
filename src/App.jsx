02b99a09e50b5ac291de70abe5b7e0a68e248c8b
AnastasiiaKa-com <nastya.karpenko.20152012@gmail.com>
2025-01-19 14:06:10 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Lesson01 from "./lessons/lesson01/Lesson01";
+import './App.css'
+import Lesson02 from "./lessons/lesson02/Lesson02";
+
+function App() {
+  return (
+    <>
+      <Lesson02 />
+    </>
+  );
+}
+
+export default App;

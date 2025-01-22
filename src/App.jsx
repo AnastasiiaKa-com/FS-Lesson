e7e0889f893adade02ac45ab78630e686d48b567
mirahmad <mizahmadm@gmail>
2025-01-22 17:13:07 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,15 @@ import './App.css'
 import Lesson02 from "./lessons/lesson02/Lesson02";
 import Lesson03 from "./lessons/lesson03/Lesson03";
 import MyButton from "./components/myButton/MyButton";
+import Lesson04 from "./lessons/lesson04/lesson04";
+import Lesson05 from "./lessons/lesson05/Lesson05";
 
 function App() {
   return (
     <>
-     <Lesson03 />
+     <Lesson04/>
+     
+     
      
     </>
   );
